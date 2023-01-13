22c87f3c9a2566fb94957cab48f3c93da9aa441f
Solaz <dan.solaz@gmail.com>
2023-01-13 04:04:58 +0200

    pass

--- server.js
+++ server.js
@@ -7,13 +7,27 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("public"));
 
+function passwordProtected(req, res, next) {
+  res.set("WWW-Authenticate", "basic realm='our MERN App'");
+  if (req.headers.authorization == "Basic ZGFuOmJpZ2RpY2s=") {
+    console.log("wat");
+    next();
+  } else {
+    console.log(req.headers.authorization);
+    res.status(401).send("try again bitch");
+  }
+}
+
 app.get("/", async (req, res) => {
   const allIdeas = await db.collection("weird_ideas").find().toArray();
   console.log(allIdeas);
   res.render("home", { allIdeas });
   //res.send(`<h1>Welcome kushim!</h1> ${allIdeas.map((idea) => `<p> ${idea.name} \n\n ${idea.time} </p>`).join("")}`);
 });
-app.get("/admin", (req, res) => {
+
+app.use(passwordProtected);
+
+app.get("/admin", passwordProtected, (req, res) => {
   res.render("admin");
 });
 app.get("/myideas/shitty", async (req, res) => {
